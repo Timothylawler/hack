474a5a0253d6ef472900230534a3657262206dc0
Exjobb <exjobb@exjobbs-air.eduroam.miun.se>
2017-04-08 09:12:26 +0200

    add front page

---
+++ index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+	
+	const mapsKey = "AIzaSyBSMMWZrPn1juxZPWR3vgz1ve7_L9YRUMk";
+	var markers = [];
+	
+	
+	$('.modal').modal();
+	
+	
+	
+	
+	function getData(){
+		let self = this;
+		
+	
+	}
+	
+	
+	
+	getData();
+	
+});
