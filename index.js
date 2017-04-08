eeb2e9d1866c4998524c1cd9f008c88cc2d12aac
Exjobb <exjobb@exjobbs-air.eduroam.miun.se>
2017-04-08 12:32:00 +0200

    add animations

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
 	
 	const mapsKey = "AIzaSyBSMMWZrPn1juxZPWR3vgz1ve7_L9YRUMk";
 	var markers = [];
+	var currentMap = 0;
+	var data = [];
+	var kmls = [
+		"CasinoCosmopol",
+		"SundsvallsKommun",
+		"Universitet"
+	]
 	
 	getServerData();
 	
@@ -10,8 +17,38 @@ $(document).ready(function(){
 		complete: closeModal
 	});
 	
+	$("#nextMapButton").click(function(){
+		$('#map').addClass("slideMap");
+		setTimeout(function(){
+			$("#map").removeClass("slideMap");
+		}, 300);
+		
+		setTimeout(function(){
+			
+		
+			var uluru = {lat: 62.3897628, lng: 17.315805};
+			document.getElementById('map').innerHTML = "";
+			var map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 11,
+				center: uluru,
+
+				disableDefaultUI: true
+			});
+			if(currentMap == 2){
+				currentMap = -1;
+			}
+			var path = 'http://northack.crystalalarm.se/kml/' + kmls[++currentMap] + '.kml????';
+			console.log(path);
+			var ctaLayer = new google.maps.KmlLayer({
+				url: path,
+				map: map
+			});
+			doStuff();
+		}, 200);
+	});
+	
 	$('#acceptBtn').click(function(){
-		sessionStorage.setItem("currentRoute", 0);
+		sessionStorage.setItem("currentRoute", currentMap);
 		window.location.href = 'http://localhost:50417/route/route.html';
 	});
 	
@@ -36,14 +73,16 @@ $(document).ready(function(){
 	function getServerData(){
 		$.ajax({url: "http://northack.crystalalarm.se/api/?lati=62.417949189425&longi=17.327530914444", success: function(result){
 			console.log("ruttisar", result);
-			doStuff(result);
+			data = result;
+			doStuff();
+			
 		}});
 	}
 	
-	function doStuff(data){
-		console.log("DATIS: ", data);
-		document.getElementById("pageTitle").innerHTML = data[0].name;
-		document.getElementById("modal-header").innerHTML =data[0].name;
+	function doStuff(){
+		//console.log("DATIS: ", data);
+		document.getElementById("pageTitle").innerHTML = data[currentMap].name;
+		document.getElementById("modal-header").innerHTML =data[currentMap].name;
 		
 	}
 	
