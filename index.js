87e2e1947bbb01c9cef30f49225b035554389064
Exjobb <exjobb@exjobbs-air.eduroam.miun.se>
2017-04-08 11:04:27 +0200

    add some data to index

--- index.js
+++ index.js
@@ -4,16 +4,45 @@ $(document).ready(function(){
 	const mapsKey = "AIzaSyBSMMWZrPn1juxZPWR3vgz1ve7_L9YRUMk";
 	var markers = [];
 	
+	getServerData();
 	
-	$('.modal').modal();
+	$('.modal').modal({
+		complete: closeModal
+	});
 	
 	$('#acceptBtn').click(function(){
 		sessionStorage.setItem("currentRoute", 0);
 		window.location.href = 'http://localhost:52884/route/routes';
 	});
 	
-	function doStuff(data){
+	$('#showModalBtn').click(function(){
+		$("#acceptBtn").addClass("hidden");
+		$("#nextMapButton").addClass("hidden");
+		$("#showModalBtn").addClass("hidden");
+	});
+	
+	$('#hideModalBtn').click(function(){
+		$('#modal1').modal('close');
+		closeModal();
+	});
+	
+	function closeModal(){
 		
+		$("#acceptBtn").removeClass("hidden");
+		$("#nextMapButton").removeClass("hidden");
+		$("#showModalBtn").removeClass("hidden");
+	}
+	
+	function getServerData(){
+		$.ajax({url: "http://northack.crystalalarm.se/api/?lati=62.417949189425&longi=17.327530914444", success: function(result){
+			console.log("ruttisar", result);
+			doStuff(result);
+		}});
+	}
+	
+	function doStuff(data){
+		console.log("DATIS: ", data);
+		document.getElementById("pageTitle").innerHTML = data[0].name;
 	}
 	
 });
