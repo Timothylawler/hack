b8bd93d5cbcbe5f9ba74b6fb465161c27d55d2d7
jool1403 <jool1403@student.miun.se>
2017-04-08 09:10:44 +0200

    Commit incase

---
+++ route.js
@@ -0,0 +1,21 @@
+
+$(function(){
+	getData();
+	
+	fixIcons();
+})
+
+
+function getData(){
+	/*
+		Insert ajax here
+	*/
+}
+
+function fixIcons(){
+	
+	document.getElementById("coffeeIcon");
+	document.getElementById("waterIcon").style.fill = "#dcdcdc";
+	document.getElementById("drinkIcon");
+	
+}
